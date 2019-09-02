f05e928e1ca58276973a6b36c9043a09b523aed8
Ivan Kazakov <i.malchugan@gmail.com>
2019-09-02 17:27:52 +0300

    Full terraform-2 lab

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable project {
+    description = "Project ID"
+}
+variable region {
+    description = "Region"
+    default = "europe-west1"
+}
+variable zone {
+    description = "Zone of instance"
+}
